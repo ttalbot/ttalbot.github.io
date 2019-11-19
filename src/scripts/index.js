b3b662e243455622b62440303b5726e9bfb6c6b9
Thomas Talbot <thomas.talbot@gmail.com>
2019-11-19 21:55:46 +0100

    update to gsap 3

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,13 @@
 import '../styles/index.scss';
 
-import { TimelineLite } from 'gsap/all';
+import { gsap } from 'gsap';
 import Splitting from "splitting";
 
 // const plugins = [ CSSPlugin ];
 
 Splitting();
-var tl = new TimelineLite();
-tl.staggerFrom('.word', 2, {y:-200, ease:Power4.easeOut},0.3, 0)
-    // .staggerFrom('.line', 2, {width: 0, ease:Power4.easeOut},0.3, 0)
-    .from('.linep1', 2, {x:500, ease:Power4.easeOut}, 0)
-    .from('.linep2', 2, {x:-1400, ease:Power4.easeOut}, 0);
+var tl = gsap.timeline({ defaults: {duration: 2, ease:"power4.out"} });
+tl.from('.word', {stagger: 0.3, y:-200}, 0)
+    // .staggerFrom('.line', 2, {width: 0},0.3, 0)
+    .from('.linep1', {x:500}, 0)
+    .from('.linep2', {x:-1400}, 0);
